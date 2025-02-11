01d41dc02b101f451753653150776eb6b6a1fa9b
velemon <143354515+velemon@users.noreply.github.com>
2025-02-12 00:27:37 +0100

    funktion som laddar datan

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,25 @@
 "use strict";
 
-let codeEl = [];
-let courseNameEl = [];
-let progresionEl = [];
+let courses = [];
 
-window.onload() => {
+window.onload = () => {
     loadCourses();
 
     //Händelselyssnare som filtrerar från sök
     document.querySelector("#search").addEventListener("input", filterData);
+}
+
+async function loadCourses() {
+    try {
+        const response = await fetch("https://webbutveckling.miun.se/files/ramschema_ht24.json");
+
+        if (!response.ok) {
+            throw new Error("Fel vid hämtning av data.")
+        }
+        courses = await response.json();
+        printCourses(courses);
+
+    } catch (error) {
+        document.querySelector("error").innerHTML = "<p>Fel vid hämtning; prova igen senare!</p>"
+    }
 }
