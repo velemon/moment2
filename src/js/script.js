42624e0a0c86a9e054d4c7fcb5bb20fca0c65521
velemon <143354515+velemon@users.noreply.github.com>
2025-02-12 00:09:06 +0100

    händelsehanterare som filtrerar

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,9 @@ let codeEl = [];
 let courseNameEl = [];
 let progresionEl = [];
 
+window.onload() => {
+    loadCourses();
+
+    //Händelselyssnare som filtrerar från sök
+    document.querySelector("#search").addEventListener("input", filterData);
+}
